9b2e7c6b88f971ecc3cbd0e45cd60fd60692692d
Raj Patel <raj202patel@gmail.com>
2025-06-11 15:16:53 -0600

    Added JS and CSS

---
+++ script.js
@@ -0,0 +1,10 @@
+// Shrink navbar on scroll
+window.addEventListener("scroll", function () {
+     const navbar = document.querySelector(".navbar");
+     if (window.scrollY > 50) {
+       navbar.classList.add("navbar-shrink");
+     } else {
+       navbar.classList.remove("navbar-shrink");
+     }
+});
+   
