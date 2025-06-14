715d3d340975e577b6ff44fffe2f40b3f772adc5
Raj Patel <raj202patel@gmail.com>
2025-06-13 20:45:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,30 @@ window.addEventListener("scroll", function () {
        navbar.classList.remove("navbar-shrink");
      }
 });
-   
+
+document.querySelectorAll('.flip-card').forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle('flipped');
+  });
+});
+
+// Auto-scroll and arrow scroll script 
+const scrollGallery = document.getElementById("scrollGallery");
+const scrollLeftBtn = document.getElementById("scrollLeft");
+const scrollRightBtn = document.getElementById("scrollRight");
+
+scrollLeftBtn.addEventListener("click", () => {
+scrollGallery.scrollBy({ left: -300, behavior: "smooth" });
+});
+
+scrollRightBtn.addEventListener("click", () => {
+scrollGallery.scrollBy({ left: 300, behavior: "smooth" });
+});
+
+// Auto-scroll every 2 seconds
+// setInterval(() => {
+// scrollGallery.scrollBy({ left: 300, behavior: "smooth" });
+// if (scrollGallery.scrollLeft + scrollGallery.clientWidth >= scrollGallery.scrollWidth - 10) {
+//      scrollGallery.scrollTo({ left: 0, behavior: "smooth" });
+// }
+// }, 2000);
